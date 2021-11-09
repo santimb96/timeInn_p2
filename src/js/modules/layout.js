0880654892581ff942e482996f2587796c4bfd1b
oasiao <74007996+oasiao@users.noreply.github.com>
2021-11-09 16:08:08 +0100

    Plantilla del header v1.0.0

--- src/js/modules/layout.js
+++ src/js/modules/layout.js
@@ -1,5 +1,18 @@
 const layout = {
-        header: `<header><h1>Header</h1></header>`,
+        header: `<div class="contenedor">
+    <div class="header">
+        <div class="logo">
+            <img src="../img/logoPalmimax.png" alt="Logo" width="200" height="60">
+        </div>
+        <div class="menu">
+            <input type="button" value="Novedades"/>
+            <input type="button" value="Cartelera"/>
+            <input type="button" value="Promociones"/>
+            <input type="button" value="Zona socio"/>
+        </div>
+    </div>
+
+</div>`,
         footer:`<footer><h4>Footer</h4></footer>`,
         render: function () {
             document.getElementById("header").innerHTML = this.header;
