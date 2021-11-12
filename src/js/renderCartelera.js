5b1ab8556c2010e74d18eb1489d85c6eed4749f4
oasiao <74007996+oasiao@users.noreply.github.com>
2021-11-12 19:43:26 +0100

    empezando cartelera.html

--- src/js/renderCartelera.js
+++ src/js/renderCartelera.js
@@ -1,15 +1,20 @@
 import {cartelera} from './modules/cartelera.js';
 
 const renderCartelera = {
-    cartelera: document.getElementById('cartelera'),
+    cartelera: document.querySelector('.cartelera'),
 
     renderCartelera: function () {
         cartelera.forEach( pelicula => {
             this.cartelera.innerHTML += `<div class="pelicula">
-                                            <img src="${pelicula.Poster}" alt="${pelicula.Title}" width="200" height="300">
-                                            <p class="titulo">${pelicula.Title}</p>
-                                            <p class="horarios">15:50</p>
-                                            <p class="horarios">20:25</p>
+                                            <img src="${pelicula.Poster}" alt="${pelicula.Title}">
+                                            <div class="text-content">
+                                                <p class="titulo">${pelicula.Title}</p>
+                                                <div class="horarios">
+                                                    <button>15:50</button>
+                                                    <button>20:25</button>                                                
+                                                </div>
+                                            </div>
+                                            
                                         </div>`;
         });
     }
