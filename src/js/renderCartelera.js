440c7bda17c03b3641a4de89c7cf6fa199777139
Santiago <Martnez>
2021-11-13 21:18:37 +0100

    feature/santiCartelera

--- src/js/renderCartelera.js
+++ src/js/renderCartelera.js
@@ -4,19 +4,21 @@ const renderCartelera = {
     cartelera: document.querySelector('.cartelera'),
 
     renderCartelera: function () {
-        cartelera.forEach( pelicula => {
+        cartelera.forEach(pelicula => {
             this.cartelera.innerHTML += `<div class="pelicula">
                                             <div class="img-container"><img src="${pelicula.Poster}" alt="${pelicula.Title}"></div>
+                                            
                                             <div class="text-content">
                                                 <h2 class="titulo-pelicula">${(pelicula.Title).toUpperCase()}</h2>
                                                 <h3 class="subtitulo-pelicula">${(pelicula.Genre).toLowerCase()} / 
                                                 ${(pelicula.Year).toLowerCase()} / ${(pelicula.Runtime).toLowerCase()}</h3>
-                                                <div class="horarios">
+                                            
+                                            <br><div class="horarios">DIGITAL
                                                     <button>15:50</button>
                                                     <button>20:25</button>                                                
-                                                </div>
                                             </div>
-                                        </div>`;
+                                            </div>
+                                         </div>`;
         });
     }
 }
