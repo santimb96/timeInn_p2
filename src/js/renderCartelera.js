4c7c624e0f9898a25e6a6a4c5e2467885bf0a3a4
Santiago <Martnez Bota>
2021-11-15 20:47:04 +0100

    js editar y borrar

--- src/js/renderCartelera.js
+++ src/js/renderCartelera.js
@@ -4,8 +4,10 @@ const renderCartelera = {
     cartelera: document.querySelector('.cartelera'),
 
     renderCartelera: function () {
+        let contador = 1;
+
         cartelera.forEach(pelicula => {
-            this.cartelera.innerHTML += `<div class="pelicula">
+            this.cartelera.innerHTML += `<div id="${contador}" class="pelicula" name="${pelicula.Title}" >
                                             <div class="img-container" name="${(pelicula.Title).toLowerCase()}" ><img src="${pelicula.Poster}" alt="${pelicula.Title}"></div>
                                             
                                             <div class="text-content">
@@ -17,17 +19,62 @@ const renderCartelera = {
                                                     <button>15:50</button>
                                                     <button>20:25</button>                                                
                                             </div>
-                                            <div class="edicion">
-                                            <button>EDITAR</button>
-                                            <button onclick="this.borrarCarta()">BORRAR</button>
+                                            <div class="ediciones">
+                                            <button id="${contador}" class="edicion" name="editar">EDITAR</button>
+                                            <button id="${contador}" class="edicion" name="borrar">BORRAR</button>
                                             </div>
                                             </div>
                                          </div>`;
+            contador++;
+        });
+
+    },
+
+    borrarListener : function () {
+
+        const botones = document.querySelectorAll('.edicion');
+        const peliculas = document.querySelectorAll('.pelicula');
+
+        botones.forEach(boton => {
+            boton.addEventListener('click', function () {
+                peliculas.forEach(pelicula => {
+                    if(boton.getAttribute('id') === pelicula.getAttribute('id')){
+                       if(boton.getAttribute('name') === 'borrar'){
+                           this.borrarCarta(pelicula);
+                       } else {
+                           this.editarCarta(pelicula.getAttribute('name'));
+                       }
+                    }
+                })
+            }.bind(this))
         });
     },
 
-    borrarCarta : function () {
+    borrarCarta: function (carta) {
+        carta.remove();
+    },
 
-    }
+    editarCarta: function(pelicula) {
+        //TODO: HACER FORMULARIO PARA INTEGRAR PELÍCULAS
+        cartelera.forEach(carta => {
+            if(carta.Title === pelicula){
+                /**
+                 * preguntar los datos en esta sección
+                 * @type {string}
+                 */
+                carta.Title = "Tus muertos"
+                this.cartelera.innerHTML = "";
+                this.renderCartelera()
+            }
+        });
+
+        /**
+         * con este código comprobamos que se añade un
+         */
+    },
+    anadirElemento: function (){}
 }
+
+
 renderCartelera.renderCartelera();
+renderCartelera.borrarListener()
