fea903e83ef75b33b84049e4d18c9e2a441d1088
Santiago <Martnez Bota>
2021-11-19 19:26:39 +0100

    form arreglado (falta hacer blur)

--- src/js/cargarSuscripcion.js
+++ src/js/cargarSuscripcion.js
@@ -5,13 +5,15 @@ const suscripcion = {
     render: function () {
 
         return `
+            <div class="form-sub">
             <div class="cerrar-boton">
                 <button class="boton-cerrar"> X</button>
             </div>
             <form name="form" id="form">
                 Suscripción <input type="text" name="suscripcion" placeholder="E-mail">
                 <button id="submit" name="submit" value="Enviar">Enviar</button>
-            </form>`;
+            </form>
+            </div>`;
     },
 
     cargaForm: function () {
@@ -23,16 +25,16 @@ const suscripcion = {
 
             this.sub.classList.add('suscripcion-mostrar');
 
+            this.cerrarSuscripcion();
+
         }, 5000);
     },
 
     cerrarSuscripcion: function () {
-        document.querySelector('.boton-cerrar').addEventListener('click', function () {
+        document.querySelector('.boton-cerrar').onclick = function () {
             this.sub.classList.remove('suscripcion-mostrar');
-        })
+        }.bind(this);
     }
 }
 
 suscripcion.cargaForm();
-suscripcion.display();
-suscripcion.cerrarSuscripcion();
