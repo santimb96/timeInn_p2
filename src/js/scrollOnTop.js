96c6b93c6979a1da75b6abd40ce2614d40554765
Santiago <Martnez Bota>
2021-11-26 01:21:40 +0100

    comenatrios TODO

--- src/js/scrollOnTop.js
+++ src/js/scrollOnTop.js
@@ -1,9 +1,21 @@
+/**
+ * @author oasiao@cifpfbmoll.eu, smartinez@cifpfbmoll.eu
+ * @version 1.0.0
+ */
+/**
+ * detecta cuando la ventana se mueve para determinar el scroll
+ */
 window.onscroll = function () {
     scrollOnTop.visualizarScroll()
 }
-
+/**
+ * objeto que determina cuándo ha de aparecer el scroll
+ * @type {{visualizarScroll: scrollOnTop.visualizarScroll, scrollTop: scrollOnTop.scrollTop}}
+ */
 const scrollOnTop = {
-
+    /**
+     * visualiza el scroll en pantalla al superar el scroll un tamaño X
+     */
     visualizarScroll: function () {
 
         const boton = document.querySelector('.scroll');
@@ -14,7 +26,9 @@ const scrollOnTop = {
             boton.style.display = "none";
         }
     },
-
+    /**
+     * botón que permite que el scroll vaya al principio mediante un botón
+     */
     scrollTop: function () {
         document.querySelector('.scroll').addEventListener('click', function () {
             document.body.scrollTop = 0;
