39d21c86b79acbd829d1c471e46e5839922e5868
oasiao <74007996+oasiao@users.noreply.github.com>
2021-12-01 14:04:13 +0100

    logIn form

--- src/js/modules/login.mjs
+++ src/js/modules/login.mjs
@@ -5,14 +5,18 @@ export const login = {
     validarTodo: function (email, password) {
         const emailPattern = /^[A-Za-z]{1}[a-z]+@[a-z]{5,10}\.(com|net|gov)+$/;
         const passwordPattern = /^[a-zA-Z]{1}[a-zA-Z0-9]{6,8}[0-9]{2}$/;
+        let validated = false;
+        let user = "";
 
         if(emailPattern.test(email) && passwordPattern.test(password)){
             usuarios.forEach(usuario => {
-                if(usuario[0] === email && usuario[1] === password){
+                if(usuario[0] === email && usuario[2] === password){
                     console.log("guay!");
-                    return [usuario.name, true];
+                    return [usuario.name,true];
                 }
             })
+        }
+        else{
             return false;
         }
     },
--- src/js/validaciones.js
+++ src/js/validaciones.js
@@ -1,7 +1,5 @@
 import {login} from "./modules/login.mjs";
 
-
-
 const validaciones = {
     email: document.querySelector('.email'),
     password: document.querySelector('.password'),
@@ -10,8 +8,8 @@ const validaciones = {
     signUp: document.querySelector('.registrar'),
 
     validar: function () {
-        const obtenerUsuario = login.validarTodo(this.email.value, this.password.value);
         this.login.addEventListener('click', function (){
+            const obtenerUsuario = login.validarTodo(this.email.value, this.password.value);
             if(obtenerUsuario[1]){
                 document.cookie = `username=${obtenerUsuario[0]}; expires=Fri, 17 Dec 2021 12:00:00 UTC`;
                 location.href = '../index.html';
@@ -38,7 +36,7 @@ const validaciones = {
         }.bind(this));
 
         this.login.addEventListener('click', function () {
-            if(login.validarAll(this.email.value, changeVerde)){
+            if(login.validarTodo(this.email.value, changeVerde)){
                 console.log("guay");
             } else {
                 console.error('error!');
