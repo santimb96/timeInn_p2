f1d4c34d4556c46ecf37352f36125401063f3a58
oasiao <74007996+oasiao@users.noreply.github.com>
2021-12-01 14:49:52 +0100

    validar v.0.2

--- src/js/modules/login.mjs
+++ src/js/modules/login.mjs
@@ -1,14 +1,14 @@
 import {usuarios} from "./usuarios.mjs";
 
 export const login = {
-
+    emailPattern: /^[A-Za-z]{1}[a-z]+@[a-z]{5,10}\.(com|net|gov)+$/,
+    passwordPattern: /^[a-zA-Z]{1}[a-zA-Z0-9]{6,8}[0-9]{2}$/,
     validarTodo: function (email, password) {
-        const emailPattern = /^[A-Za-z]{1}[a-z]+@[a-z]{5,10}\.(com|net|gov)+$/;
-        const passwordPattern = /^[a-zA-Z]{1}[a-zA-Z0-9]{6,8}[0-9]{2}$/;
+
         let validated = false;
         let user = "";
 
-        if(emailPattern.test(email) && passwordPattern.test(password)){
+        if(this.validarEmail(email) && this.validarPassword(password)){
             usuarios.forEach(usuario => {
                 if(usuario.email === email && usuario.password === password){
                     user = usuario.name;
@@ -18,4 +18,10 @@ export const login = {
         }
         return [user,validated];
     },
+    validarPassword: function (password){
+        return this.passwordPattern.test(password);
+    },
+    validarEmail: function (email){
+        return this.emailPattern.test(email);
+    }
 }
--- src/js/validaciones.js
+++ src/js/validaciones.js
@@ -9,15 +9,33 @@ const validaciones = {
 
     validar: function (){
         this.login.addEventListener('click',function (){
-            if (login.validarTodo(this.email.value,this.password.value)){
-                console.log("logged");
+            let user = login.validarTodo(this.email.value,this.password.value)
+            if (user[1]){
+                document.cookie = `username=${user[0]}; expires=Fri, 17 Dec 2021 12:00:00 UTC`;
+                location.href = 'index.html';
+                console.log('loggeado!');
             }
             else
             {
                 console.log("not logged");
             }
         }.bind(this));
+    },
+    validarRegistro: function () {
+        let changeVerde = false;
+        this.password.addEventListener('change', function () {
+            if(login.validarPassword(this.password.value)){
+                this.password.classList.add('campoCorrecto');
+                changeVerde = true;
+            } else {
+                this.password.classList.add('campoVacio');
+                changeVerde = false;
+            }
+        }.bind(this));
     }
 }
 
 validaciones.validar();
+validaciones.validarRegistro();
+
+
